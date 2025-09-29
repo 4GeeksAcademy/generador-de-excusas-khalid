3b14cce9374a8630ac812c11ae35f1ad2295a15a
kelkamel94 <kelkamel94@gmail.com>
2025-09-29 19:36:16 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -7,10 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generateSentence(){
+
+const randomWho = who[Math.floor(Math.random() * who.length)];
+const randomAction = action[Math.floor(Math.random() * action.length)];
+const randomWhat = what[Math.floor(Math.random() * what.length)];
+const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+
+
+}
+document.getElementById('excuse').innerHTML = generateSentence();
+
 
 
 
